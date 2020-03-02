aa8735475b129fa14d7d16506f46ccdb56db0e56
Alexandre HOSSEIN <alex.hos.27@gmail.com>
2020-03-02 11:16:34 +0100

    Ajout directory assets/js/function
    Ajout ajax.js
    
    création fonction connect()

---
+++ assets/js/ajax.js
@@ -0,0 +1,16 @@
+function connect() {
+    if (window.XMLHttpRequest){
+        xhr = new XMLHttpRequest(); // si oui je crée une instance
+    } else if (window.ActiveXObject) {
+        try {
+            xhr = new ActiveXObject("Msxml12.XMLHTTP");
+        }
+        catch (e) {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    } else {
+        alert('Mettez à jour votre navigateur');
+        xhr = false;
+    }
+    return xhr
+}
